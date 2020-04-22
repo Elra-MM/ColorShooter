d499c3f65abcd263b1510aadb40d7f0956066871
canu1337 <treisska@gmail.com>
2020-04-23 00:33:14 +0200

    Add colision detectio before spawning an enemy, preventing enemy stacking

--- Assets/Scripts/EnemiesGenerator.cs
+++ Assets/Scripts/EnemiesGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemiesGenerator : MonoBehaviour
@@ -16,12 +17,12 @@ public class EnemiesGenerator : MonoBehaviour
     private readonly Vector3 rotationDown = new Vector3(0, 0, 0);
     private readonly Vector3 rotationLeft = new Vector3(0, 90, 0);
     private readonly Vector3 rotationRight = new Vector3(0, 270, 0);
-    private List<Tuple<Vector3, Vector3>> positionsRotations;
+    private List<Tuple<Vector3, Vector3>> spawns;
 
     // Start is called before the first frame update
     void Start()
     {
-        positionsRotations = new List<Tuple<Vector3, Vector3>>
+        spawns = new List<Tuple<Vector3, Vector3>>
         {
             Tuple.Create(positionUp, rotationUp),
             Tuple.Create(positionDown, rotationDown),
@@ -36,13 +37,30 @@ public class EnemiesGenerator : MonoBehaviour
     {
         while (true)
         {
-            var enemyPrefab = EnemiesPrefabs[UnityEngine.Random.Range(0, EnemiesPrefabs.Count)];
-            var positionRotation = positionsRotations[UnityEngine.Random.Range(0, positionsRotations.Count)];
-            Instantiate(enemyPrefab, positionRotation.Item1, Quaternion.Euler(positionRotation.Item2));
+            var availabeSpawns = GetAvailableSpawns();
+            if (availabeSpawns.Any())
+            {
+                var enemyPrefab = EnemiesPrefabs[UnityEngine.Random.Range(0, EnemiesPrefabs.Count)];
+                var positionRotation = availabeSpawns[UnityEngine.Random.Range(0, availabeSpawns.Count)];
+                Instantiate(enemyPrefab, positionRotation.Item1, Quaternion.Euler(positionRotation.Item2));
+            }
 
             yield return new WaitForSeconds(SecondsBetweenEnnemies);
         }
 
+    }
+
+    private List<Tuple<Vector3, Vector3>> GetAvailableSpawns()
+    {
+        var res = new List<Tuple<Vector3, Vector3>>();
+        foreach(var spawn in spawns)
+        {
+            if (!Physics.OverlapSphere(spawn.Item1, 1f).Any())
+            {
+                res.Add(spawn);
+            }
+        }
+        return res;
     }
 
     // Update is called once per frame
