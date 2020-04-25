e161e18d4dae14845c7b89d3a43e10f463645304
Morgane <morgane@opuscope.com>
2020-04-25 22:10:05 +0200

    Clean

--- Assets/Scripts/LivesCounter.cs
+++ Assets/Scripts/LivesCounter.cs
@@ -6,18 +6,11 @@ public class LivesCounter : MonoBehaviour
 {
     public int CurrentLives;
     private const string enemyTag = "Enemy";
-    // Start is called before the first frame update
+    
     void Start()
     {
         CurrentLives = 3;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag(enemyTag))
