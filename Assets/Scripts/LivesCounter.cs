a38673d00ba844b89cf639232a0708107f69c2a2
canu1337 <treisska@gmail.com>
2020-04-25 21:38:38 +0200

    Add lives loss on hit

---
+++ Assets/Scripts/LivesCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesCounter : MonoBehaviour
+{
+    public int CurrentLives;
+    private const string enemyTag = "Enemy";
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentLives = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(enemyTag))
+        {
+            CurrentLives--;
+            Destroy(collision.gameObject);
+        }
+    }
+}
