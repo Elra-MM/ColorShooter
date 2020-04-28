04722cf504fd4117a23a6864db1de9391cb087ae
canu1337 <treisska@gmail.com>
2020-04-28 23:18:56 +0200

    Add main menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject MainMenuPanel;
+    public void Awake()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void TryAgain()
+   {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+      Time.timeScale = 1;
+   }
+
+    public void StartGame()
+    {
+        MainMenuPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
